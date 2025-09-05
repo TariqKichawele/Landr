dc3663e4beb8b353d9be6ba20611cdc2a5d3a935
TariqKichawele <tariqkichawele66@gmail.com>
2025-09-05 13:42:27 -0400

    05: Implement Navbar and User Avatar components
    - Added a new Navbar component for user navigation, integrating user profile and logout functionality.
    - Created UserAvatar component to display user images with fallback initials.
    - Updated layout to include the Navbar component.
    - Added @radix-ui/react-avatar dependency for avatar functionality.
    - Updated package.json and package-lock.json to reflect new dependencies.

---
+++ src/app/app/_navbar.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { SignOutButton, useClerk } from "@clerk/nextjs";
+import { BookOpenIcon, BrainCircuitIcon, FileSlidersIcon, LogOut, SpeechIcon, User } from "lucide-react";
+import { useParams, usePathname } from "next/navigation";
+import Link from "next/link";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { UserAvatar } from "@/features/users/components/userAvatar";
+
+const navLinks = [
+    { name: "Interviews", href: "interviews", Icon: SpeechIcon },
+    { name: "Questions", href: "questions", Icon: BookOpenIcon },
+    { name: "Resume", href: "resume", Icon: FileSlidersIcon },
+];
+
+export function Navbar({ user }: { user: { name: string, imageUrl: string } }) {
+    const { openUserProfile } = useClerk();
+    const { jobInfoId } = useParams();
+    const pathName = usePathname();
+
+
+    return (
+        <nav className="h-header border-b">
+            <div className="container flex h-full items-center justify-between">
+                <Link href="/app" className="flex items-center gap-2">
+                    <BrainCircuitIcon className="size-8 text-primary" />
+                    <span className="text-xl font-bold">Landr</span>
+                </Link>
+        
+                <div className="flex items-center gap-4">
+
+        
+                    <ThemeToggle />
+        
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <UserAvatar user={user} />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" className="w-56">
+                            <DropdownMenuItem onClick={() => openUserProfile()}>
+                                <User className="mr-2" />
+                                Profile
+                            </DropdownMenuItem>
+                            <SignOutButton>
+                                <DropdownMenuItem>
+                                    <LogOut className="mr-2" />
+                                    Logout
+                                </DropdownMenuItem>
+                            </SignOutButton>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            </div>
+        </nav>
+    )
+}
