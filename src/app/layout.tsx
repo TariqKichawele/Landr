9a1cafb7df4d471d62a55799f6589f2a5a5338ac
TariqKichawele <tariqkichawele66@gmail.com>
2025-09-06 10:47:50 -0400

    07: Update dependencies and enhance job information features
    - Added new dependencies: @hookform/resolvers, @radix-ui/react-label, @radix-ui/react-select, react-hook-form, and sonner.
    - Updated zod version in package.json.
    - Implemented JobInfoForm component for creating and updating job information.
    - Introduced new JobInfoNewPage for job creation.
    - Enhanced layout with Toaster for notifications and improved UI components for forms and selections.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@/services/clerk/components/ClerkProvider";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 
 const outfitSans = Outfit({
   variable: "--font-outfit-sans",
@@ -22,9 +23,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${outfitSans.variable} antialiased font-sans`}
-        >
+        <body className={`${outfitSans.variable} antialiased font-sans`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -32,6 +31,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
---
+++ src/features/jobInfos/actions.ts
@@ -0,0 +1,75 @@
+"use server"
+
+import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser"
+import { jobInfoSchema } from "./schema"
+import z from "zod"
+import { redirect } from "next/navigation";
+import { insertJobInfo, updateJobInfo as updateJobInfoDb } from "./db";
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { db } from "@/drizzle/db";
+import { eq, and } from "drizzle-orm";
+import { JobInfoTable } from "@/drizzle/schema";
+import { getJobInfoIdTag } from "./dbCache";
+
+export async function createJobInfo(unsafeData: z.infer<typeof jobInfoSchema>) {
+    const { userId } = await getCurrentUser({ allData: false });
+    if (userId == null) {
+        return {
+            error: true,
+            message: "Unauthorized",
+        }
+    }
+
+    const { success, data } = jobInfoSchema.safeParse(unsafeData);
+    if (!success) {
+        return {
+            error: true,
+            message: "Invalid data",
+        }
+    }
+
+    const jobInfo = await insertJobInfo({ ...data, userId });
+
+    redirect(`/app/job-infos/${jobInfo.id}`);
+}
+
+
+export async function updateJobInfo(id: string, unsafeData: z.infer<typeof jobInfoSchema>) {
+    const { userId } = await getCurrentUser({ allData: false });
+    if (userId == null) {
+        return {
+            error: true,
+            message: "Unauthorized",
+        }
+    }
+
+    const { success, data } = jobInfoSchema.safeParse(unsafeData);
+    if (!success) {
+        return {
+            error: true,
+            message: "Invalid data",
+        }
+    }
+
+    const existingJobInfo = await getJobInfo(id, userId);
+    if (existingJobInfo == null) {
+        return {
+            error: true,
+            message: "Job info not found",
+        }
+    }
+
+    const jobInfo = await updateJobInfoDb(id, data);
+
+    redirect(`/app/job-infos/${jobInfo.id}`);
+}
+
+async function getJobInfo(id: string, userId: string) {
+    "use cache"
+    cacheTag(getJobInfoIdTag(id))
+
+    return db.query.JobInfoTable.findFirst({
+        where: and(eq(JobInfoTable.id, id), eq(JobInfoTable.userId, userId)),
+    })
+}
+
